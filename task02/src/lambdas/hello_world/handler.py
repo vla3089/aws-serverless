129b57625497654a263a7370a83c0edc5db10866
Vlad <vmatelski@vladds.com>
2025-01-27 16:42:15 +0100

    update code and props

--- task02/src/lambdas/hello_world/handler.py
+++ task02/src/lambdas/hello_world/handler.py
@@ -23,6 +23,19 @@ class HelloWorld(AbstractLambda):
 
 HANDLER = HelloWorld()
 
-
 def lambda_handler(event, context):
-    return HANDLER.lambda_handler(event=event, context=context)
+    print(json.dumps(event))  # Logs the event for inspection
+    # Check if the HTTP method is GET and the path is /hello
+    if event.get("httpMethod") == "GET" and event.get("path") == "/hello":
+        return {
+            "statusCode": 200,
+            "message": "Hello from Lambda"
+        }
+    else:
+        # Bad request syntax or unsupported method. Request path: /cmtr-7e60a31c. HTTP method: GET"
+        # 
+        return {
+            "statusCode": 400,
+            "message": "Bad request syntax or unsupported method. Request path: {0}. HTTP method: {1}".format(event.get("httpMethod"), event.get("path"))
+        }
+
