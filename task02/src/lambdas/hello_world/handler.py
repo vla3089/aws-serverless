8f4f9edaa4e2dfaeb0225071d71d0525bab29cc0
Vlad <vmatelski@vladds.com>
2025-01-27 20:01:32 +0100

    implement validation

--- task02/src/lambdas/hello_world/handler.py
+++ task02/src/lambdas/hello_world/handler.py
@@ -1,31 +1,29 @@
-import json
+from commons.log_helper import get_logger
+from commons.abstract_lambda import AbstractLambda
 
-def lambda_handler(event, context):
-    print(json.dumps(event))  # Logs the event for inspection
-    # Check if the HTTP method is GET and the path is /hello
-    if event.get("httpMethod") == "GET" and event.get("path") == "/hello":
-        content = {
-            "statusCode": 200,
+_LOG = get_logger(__name__)
+
+
+class HelloWorld(AbstractLambda):
+
+    def validate_request(self, event) -> dict:
+        method = event["requestContext"]["http"]["method"]
+        path = event.get("requestContext").get("http").get("path")
+        if method != "GET" and path != "/hello":
+            return {
+                "statusCode": 400,
+                "message": "Bad request syntax or unsupported method. Request path: {0}. HTTP method: {1}".format(path, method)
+                }
+        else:
+            pass
+        
+    def handle_request(self, event, context):
+        return {
             "message": "Hello from Lambda"
         }
-        return {
-            "statusCode" : 200,
-            "headers": {
-                "Content-Type": "application/json",
-            },
-            "body": content,
-            "isBase64Encoded": False
-        }
-    else:
-        content = {
-            "statusCode": 400,
-            "message": "Bad request syntax or unsupported method. Request path: {0}. HTTP method: {1}".format(event.get("httpMethod"), event.get("path"))
-        }
-        return {
-            "statusCode" : 200,
-            "headers": {
-                "Content-Type": "application/json",
-            },
-            "body": content,
-            "isBase64Encoded": False
-        }
+    
+
+HANDLER = HelloWorld()
+
+def lambda_handler(event, context):
+    return HANDLER.lambda_handler(event=event, context=context)
