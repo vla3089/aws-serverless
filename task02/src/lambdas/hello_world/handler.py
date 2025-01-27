f328698a8654ce6033f1fc11e5f9220f8d72479c
Vlad <vmatelski@vladds.com>
2025-01-27 19:28:36 +0100

    implement lambda

--- task02/src/lambdas/hello_world/handler.py
+++ task02/src/lambdas/hello_world/handler.py
@@ -1,41 +1,31 @@
-from commons.log_helper import get_logger
-from commons.abstract_lambda import AbstractLambda
 import json
-
-_LOG = get_logger(__name__)
-
-
-class HelloWorld(AbstractLambda):
-
-    def validate_request(self, event) -> dict:
-        pass
-        
-    def handle_request(self, event, context):
-        """
-        Explain incoming event here
-        """
-        print(json.dumps(event))  # Logs the event for inspection
-        return {
-            "statusCode": 200,
-            "message": "Hello from Lambda"
-        }
-    
-
-HANDLER = HelloWorld()
 
 def lambda_handler(event, context):
     print(json.dumps(event))  # Logs the event for inspection
     # Check if the HTTP method is GET and the path is /hello
     if event.get("httpMethod") == "GET" and event.get("path") == "/hello":
-        return {
+        content = {
             "statusCode": 200,
             "message": "Hello from Lambda"
         }
-    else:
-        # Bad request syntax or unsupported method. Request path: /cmtr-7e60a31c. HTTP method: GET"
-        # 
         return {
+            "statusCode" : 200,
+            "headers": {
+                "Content-Type": "application/json",
+            },
+            "body": content,
+            "isBase64Encoded": False
+        }
+    else:
+        content = {
             "statusCode": 400,
             "message": "Bad request syntax or unsupported method. Request path: {0}. HTTP method: {1}".format(event.get("httpMethod"), event.get("path"))
         }
-
+        return {
+            "statusCode" : 200,
+            "headers": {
+                "Content-Type": "application/json",
+            },
+            "body": content,
+            "isBase64Encoded": False
+        }
