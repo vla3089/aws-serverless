bceba8ed1317b3adbc4cb00614ec5d8e1255553a
Vlad <vmatelski@vladds.com>
2025-01-27 20:12:55 +0100

    move validate back to handle

--- task02/src/lambdas/hello_world/handler.py
+++ task02/src/lambdas/hello_world/handler.py
@@ -7,25 +7,28 @@ _LOG = get_logger(__name__)
 class HelloWorld(AbstractLambda):
 
     def validate_request(self, event) -> dict:
+        pass
+        
+    def handle_request(self, event, context):
         method = event["requestContext"]["http"]["method"]
         path = event.get("requestContext").get("http").get("path")
+
         if method != "GET" and path != "/hello":
             return {
                 "statusCode": 400,
                 "message": "Bad request syntax or unsupported method. Request path: {0}. HTTP method: {1}".format(path, method)
                 }
         else:
-            pass
-        
-    def handle_request(self, event, context):
-        content = {
+            content = {
             "statusCode": 200,
             "message": "Hello from Lambda"
-        }
-        return {
-            'statusCode': 200,
-            'body': content
-        }
+            }
+            return {
+                'statusCode': 200,
+                'body': content
+            }
+
+        
     
 
 HANDLER = HelloWorld()
