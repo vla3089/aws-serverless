4f05fb1e9d56f6b28386f88787a20b7e2fefeb0e
Vlad <vmatelski@vladds.com>
2025-02-01 23:19:48 +0100

    tweak db insertion event

--- task05/src/lambdas/api_handler/handler.py
+++ task05/src/lambdas/api_handler/handler.py
@@ -1,8 +1,11 @@
 from commons.log_helper import get_logger
 from commons.abstract_lambda import AbstractLambda
+import json
+import boto3
+import uuid
+from datetime import datetime
 
 _LOG = get_logger(__name__)
-
 
 class ApiHandler(AbstractLambda):
 
@@ -10,12 +13,40 @@ class ApiHandler(AbstractLambda):
         pass
         
     def handle_request(self, event, context):
-        """
-        Explain incoming event here
-        """
-        # todo implement business logic
-        return 200
-    
+        _LOG.info(f'{event}')
+        try:
+            # Parse request body
+            content = event.get('body', '{}')
+            _LOG.info(f'Content: {content}')
+            
+            # Generate a unique ID for the event
+            event_id = str(uuid.uuid4())
+            _LOG.info(f'event_id: {event_id}')
+
+            # Prepare item for DynamoDB
+            item = {
+                'id': event_id,
+                'created_at': datetime.utcnow().isoformat(),
+                'body': json.dumps(content)
+            }
+            _LOG.info(f'Item to put to dynamodb: {item}')
+            
+            # Save to DynamoDB
+            
+            dynamodb = boto3.resource('dynamodb', region_name='eu-central-1')
+            table = dynamodb.Table('cmtr-7e60a31c-Events')
+            table.put_item(Item=item)
+            
+            # Return response
+            return {
+                'statusCode': 201,
+                'event': json.dumps({'event': item})
+            }
+        except Exception as e:
+            return {
+                'statusCode': 500,
+                'body': json.dumps({'error': str(e)})
+            }
 
 HANDLER = ApiHandler()
 
