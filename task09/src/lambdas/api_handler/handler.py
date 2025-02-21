1afc2d0b0b00a8368e768b93edc911fd71ef6294
Vlad <vmatelski@vladds.com>
2025-02-22 00:20:56 +0100

    add lambda url

--- task09/src/lambdas/api_handler/handler.py
+++ task09/src/lambdas/api_handler/handler.py
@@ -8,12 +8,34 @@ DEFAULT_LONGITUDE = 13.41
 # Open-Meteo API URL template
 WEATHER_API_URL = "https://api.open-meteo.com/v1/forecast?latitude={}&longitude={}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
 
+def map_weather_data(input_data):
+    mapped_data = {
+        "latitude": input_data.get("latitude"),
+        "longitude": input_data.get("longitude"),
+        "generationtime_ms": input_data.get("generationtime_ms"),
+        "utc_offset_seconds": input_data.get("utc_offset_seconds"),
+        "timezone": input_data.get("timezone"),
+        "timezone_abbreviation": input_data.get("timezone_abbreviation"),
+        "elevation": input_data.get("elevation"),
+        "hourly_units": input_data.get("hourly_units"),
+        "hourly": {
+            "time": input_data["hourly"]["time"],
+            "temperature_2m": input_data["hourly"]["temperature_2m"],
+            "relative_humidity_2m": input_data["hourly"].get("relative_humidity_2m", []),
+            "wind_speed_10m": input_data["hourly"]["wind_speed_10m"],
+        },
+        "current_units": input_data.get("current_units"),
+        "current": input_data.get("current"),
+    }
+    return mapped_data
+
 def lambda_handler(event, context):
+    print(event)
     """Lambda function to handle GET /weather requests"""
     
     # Validate HTTP method and path
-    method = event.get("httpMethod")
-    path = event.get("path")
+    method = event["requestContext"]["http"]["method"]
+    path = event.get("requestContext").get("http").get("path")
     if method != "GET" or path != "/weather":
         return {
             "statusCode": 400,
@@ -31,7 +53,7 @@ def lambda_handler(event, context):
         # Fetch weather data from Open-Meteo
         response = requests.get(url)
         response.raise_for_status()  # Raise an error for HTTP error responses
-        weather_data = response.json()
+        weather_data = map_weather_data(response.json())
 
         return {
             "statusCode": 200,
