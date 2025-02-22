f49e8b4db3cf240ae8edc8d53d6ef41ccdfea153
Vlad <vmatelski@vladds.com>
2025-02-22 14:32:51 +0100

    update auth

--- task11/src/lambdas/api_handler/handler.py
+++ task11/src/lambdas/api_handler/handler.py
@@ -76,10 +76,9 @@ def signup(email, password):
 
 def signin(email, password):
     try:
-        response = cognito_client.client.admin_initiate_auth(
-            UserPoolId=CUP_ID,
+        response = cognito_client.client.initiate_auth(
             ClientId=CLIENT_ID,
-            AuthFlow='ADMIN_USER_PASSWORD_AUTH',
+            AuthFlow='USER_PASSWORD_AUTH',
             AuthParameters={
                 'USERNAME': email,
                 'PASSWORD': password
